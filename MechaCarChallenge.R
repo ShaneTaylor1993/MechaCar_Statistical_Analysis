71223dba51771abf2fe543a790236c9a844a33c8
ShaneTaylor1993 <75644168+ShaneTaylor1993@users.noreply.github.com>
2022-04-13 14:07:10 -0500

    Updating R doc and README file.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,14 @@
 library(dplyr)
 library(tidyverse)
 
+#Creating Dataframe
 MechaCar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 head(MechaCar_mpg_df)
 
+#Performing Linear Regression
+lm_MecharCar_mpg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg_df)
+lm_MecharCar_mpg
+
+#Using summary() to determine p-value and r-squared value of the linear regression model
+summary_lm_MecharCar_mpg <- summary(lm_MecharCar_mpg)
+summary_lm_MecharCar_mpg
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MechaCar_Statistical_Analysis
-Performing statistical analysis using R
+
+## Overview
+
+## Linear Regression to Predict MPG
+1. Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+- 
+
+2. Is the slope of the linear model considered to be zero? Why or why not?
+- 
+
+3. Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+- 
